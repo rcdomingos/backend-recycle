b8ab4e5c3f4567fd6bdb355c5cc424b48365ee78
Reginaldo C Domingos <rcdomingos@live.com>
2020-04-30 21:33:42 -0300

    add collector model, controller and route

---
+++ src/routes/collector.routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const routes = express.Router();
+
+const authentication = require('../middlewares/authentication');
+
+const CollectorController = require('../controllers/collector.controller');
+
+/**rotas apartir do '/api/v1/collectors' */
+routes.post('/', CollectorController.apiAddCollector);
+
+routes.get('/', CollectorController.apiGetAllPreCollector);
+
+routes.delete('/:id', CollectorController.apiDeletePreCollector);
+
+routes.get('/:id/status', CollectorController.apiGetStatusPreCollector);
+
+routes.patch('/:id/status', CollectorController.apiAlterStatusPreCollector);
+
+module.exports = routes;
